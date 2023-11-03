20f0442722d88e91675fa68e6361fe3f64be9af9
sontruong012004s <9truongson9@gmail.com>
2023-11-03 19:16:00 +0700

    update file

---
+++ File Các buổi học/2023 10 25 PF-W04/script.js
@@ -0,0 +1,20 @@
+let btnSubmit = document.getElementById("btn-submit").addEventListener("click", () => {
+    let inputHours = parseInt(document.getElementById("hours").value);
+    let result = 0;
+    if(inputHours >= 400){
+        switch (parseInt(document.getElementById("list-option").value)){
+            case 1:
+                result = 1000;
+                break;
+            case 2:
+                result = 2000;
+                break;
+            default:
+                alert("Không hợp lệ");
+                break;
+        }
+        document.getElementById("result").innerHTML = `Nhân viên được thưởng: ${result} $USD`;
+    } else{
+        document.getElementById("result").innerHTML = "Nhân viên không đủ số giờ làm";
+    }
+});

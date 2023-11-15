bf8a94ad5a726a4ae1df07cee4f4d770d84b3231
Your Name <you@example.com>
2023-11-15 11:50:08 +0700

    homework

---
+++ Homework/Mảng 2023 11 13/Practice_function_homework/practice-1.js
@@ -0,0 +1,20 @@
+/**
+ * Bài 1:
+    Viết hàm sumEven() nhận vào một số `n` và in ra tổng các số chẵn từ 2 đến n.
+    Ví dụ:
+    sumEven(10)
+
+    Output:
+    30
+ */
+
+function sumEven(n){
+   let sum = 0;
+   for(let i = 2; i <= n; i++){
+      if(i %2 === 0){
+         sum += i;
+      }
+   }
+   console.log(sum);
+}
+sumEven(10);

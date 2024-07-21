3f096f49d59c1f766bce78e50be3a095dafdf54f
sontruong012004s <9truongson9@gmail.com>
2024-07-21 14:53:22 +0700

    update

--- C0524L1-RJ102/C05R/demo1/src/page/Register.js
+++ C0524L1-RJ102/C05R/demo1/src/page/Register.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import '../css/Login.css';
 export default function Register() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
